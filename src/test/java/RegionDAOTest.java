07585b8b6a0789f90eed1bddbbe9c4c0d811a048
prithviraghavtm <prithviamruthraj@gmail.com>
2018-06-02 15:44:15 +0530

    successful test

--- src/test/java/RegionDAOTest.java
+++ src/test/java/RegionDAOTest.java
@@ -1,5 +1,13 @@
 
 
+import static org.mockito.Matchers.isA;
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.times;
+
+import java.net.URISyntaxException;
+import java.security.InvalidKeyException;
+
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mock;
@@ -8,6 +16,7 @@ import org.mockito.MockitoAnnotations;
 import com.infotech.client.RegionDAO;
 import com.infotech.client.TableAccessHelper;
 import com.infotech.model.RegionModel;
+import com.microsoft.azure.storage.StorageException;
 
 public class RegionDAOTest {
 
@@ -20,11 +29,14 @@ public class RegionDAOTest {
     }
     
     @Test
-    public void test()
+    public void test() throws InvalidKeyException, URISyntaxException, StorageException
     {
     	RegionDAO dao = new RegionDAO(this.accessHelper);
     	
-    //when(accessHelper.save(any(RegionModel.class))
-    //	when(this.accessHelper.save(any(RegionModel.class)))
+    	doNothing().when(this.accessHelper).save(isA(RegionModel.class));
+
+    	dao.SaveRegion(new RegionModel("001", "abc"));
+    	
+    	verify(this.accessHelper, times(1)).save(isA(RegionModel.class));
     }
 }
